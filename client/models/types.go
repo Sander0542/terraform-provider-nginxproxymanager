b3d8c813450244d8b03cfa5df0a3f4c374ba00fe
Sander Jochems <sanderjochems@hotmail.nl>
2023-04-29 14:28:44 +0200

    Move models to models directory

---
+++ client/models/types.go
@@ -0,0 +1,37 @@
+package models
+
+import (
+	"encoding/json"
+)
+
+type boolAsInt bool
+
+type Meta map[string]json.RawMessage
+
+func (b *boolAsInt) UnmarshalJSON(data []byte) error {
+	var i int
+	if err := json.Unmarshal(data, &i); err != nil {
+		return err
+	}
+	*b = boolAsInt(i != 0)
+	return nil
+}
+
+func (b boolAsInt) MarshalJSON() ([]byte, error) {
+	if b {
+		return json.Marshal(1)
+	}
+	return json.Marshal(0)
+}
+
+func (b boolAsInt) Bool() bool {
+	return bool(b)
+}
+
+func (m *Meta) Map() map[string]string {
+	res := map[string]string{}
+	for k, v := range *m {
+		res[k] = string(v)
+	}
+	return res
+}
--- client/proxy_hosts.go
+++ client/proxy_hosts.go
@@ -3,34 +3,11 @@ package client
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/sander0542/terraform-provider-nginxproxymanager/client/models"
 	"net/http"
 )
 
-type ProxyHostResponse struct {
-	ID                    int64           `json:"id"`
-	CreatedAt             string          `json:"created_at"`
-	ModifiedOn            string          `json:"modified_on"`
-	DomainNames           []string        `json:"domain_names"`
-	ForwardScheme         string          `json:"forward_scheme"`
-	ForwardHost           string          `json:"forward_host"`
-	ForwardPort           uint16          `json:"forward_port"`
-	CertificateID         json.RawMessage `json:"certificate_id"`
-	SSLForced             boolAsInt       `json:"ssl_forced"`
-	HSTSEnabled           boolAsInt       `json:"hsts_enabled"`
-	HSTSSubdomains        boolAsInt       `json:"hsts_subdomains"`
-	HTTP2Support          boolAsInt       `json:"http2_support"`
-	BlockExploits         boolAsInt       `json:"block_exploits"`
-	CachingEnabled        boolAsInt       `json:"caching_enabled"`
-	AllowWebsocketUpgrade boolAsInt       `json:"allow_websocket_upgrade"`
-	AccessListID          int64           `json:"access_list_id"`
-	AdvancedConfig        string          `json:"advanced_config"`
-	Enabled               boolAsInt       `json:"enabled"`
-	Meta                  Meta            `json:"meta"`
-}
-
-type ProxyHostsResponse []ProxyHostResponse
-
-func (c *Client) GetProxyHosts() (*ProxyHostsResponse, error) {
+func (c *Client) GetProxyHosts() (*models.ProxyHostsResponse, error) {
 	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/nginx/proxy-hosts", c.HostURL), nil)
 	if err != nil {
 		return nil, err
@@ -41,7 +18,7 @@ func (c *Client) GetProxyHosts() (*ProxyHostsResponse, error) {
 		return nil, err
 	}
 
-	ar := ProxyHostsResponse{}
+	ar := models.ProxyHostsResponse{}
 	err = json.Unmarshal(body, &ar)
 	if err != nil {
 		return nil, err
@@ -50,7 +27,7 @@ func (c *Client) GetProxyHosts() (*ProxyHostsResponse, error) {
 	return &ar, nil
 }
 
-func (c *Client) GetProxyHost(id *int64) (*ProxyHostResponse, error) {
+func (c *Client) GetProxyHost(id *int64) (*models.ProxyHostResponse, error) {
 	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/nginx/proxy-hosts/%d", c.HostURL, *id), nil)
 	if err != nil {
 		return nil, err
@@ -61,7 +38,7 @@ func (c *Client) GetProxyHost(id *int64) (*ProxyHostResponse, error) {
 		return nil, err
 	}
 
-	ar := ProxyHostResponse{}
+	ar := models.ProxyHostResponse{}
 	err = json.Unmarshal(body, &ar)
 	if err != nil {
 		return nil, err
