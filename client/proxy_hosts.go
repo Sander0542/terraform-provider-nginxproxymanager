bbf75c11bd8f2e1b81a14c496fbc7ba9a862a52b
Sander Jochems <sanderjochems@hotmail.nl>
2023-04-29 17:55:35 +0200

    Implement CreateProxyHost

--- client/proxy_hosts.go
+++ client/proxy_hosts.go
@@ -5,7 +5,35 @@ import (
 	"fmt"
 	"github.com/sander0542/terraform-provider-nginxproxymanager/client/models"
 	"net/http"
+	"strings"
 )
+
+func (c *Client) CreateProxyHost(proxyHost *models.ProxyHostCreate) (*models.ProxyHostResponse, error) {
+	rb, err := json.Marshal(proxyHost)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/api/nginx/proxy-hosts", c.HostURL), strings.NewReader(string(rb)))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	body, err := c.doRequest(req, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	ar := models.ProxyHostResponse{}
+	err = json.Unmarshal(body, &ar)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ar, nil
+}
 
 func (c *Client) GetProxyHosts() (*models.ProxyHostsResponse, error) {
 	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/nginx/proxy-hosts", c.HostURL), nil)
