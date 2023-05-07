55e72eb9aa45e885ef4605bf8985cd59a96d1fcc
Sander Jochems <sanderjochems@hotmail.nl>
2023-05-07 23:51:51 +0200

    Fix transaction names

--- nginxproxymanager/sentry/main.go
+++ nginxproxymanager/sentry/main.go
@@ -18,9 +18,9 @@ func CaptureDiagnostics(diags diag.Diagnostics) {
 }
 
 func StartResource(ctx context.Context, name string, operation string) *sentry.Span {
-	return sentry.StartSpan(ctx, fmt.Sprintf("terraform.resource.%s", operation), sentry.TransactionName(fmt.Sprintf("resource.%s.read", name)))
+	return sentry.StartSpan(ctx, fmt.Sprintf("terraform.resource.%s", operation), sentry.TransactionName(fmt.Sprintf("resource.%s.%s", name, operation)))
 }
 
 func StartDataSource(ctx context.Context, name string, operation string) *sentry.Span {
-	return sentry.StartSpan(ctx, fmt.Sprintf("terraform.data_source.%s", operation), sentry.TransactionName(fmt.Sprintf("data.%s.read", name)))
+	return sentry.StartSpan(ctx, fmt.Sprintf("terraform.data_source.%s", operation), sentry.TransactionName(fmt.Sprintf("data.%s.%s", name, operation)))
 }
