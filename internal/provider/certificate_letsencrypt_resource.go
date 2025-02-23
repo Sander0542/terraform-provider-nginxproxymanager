ec1874c593dd840bcce91c92a942fb877d3d6b52
Sander Jochems <sanderjochems@hotmail.nl>
2025-02-23 12:09:09 +0100

    Implement letsencrypt certificate resource

---
+++ internal/provider/certificate_letsencrypt_resource.go
@@ -0,0 +1,235 @@
+// Copyright (c) Sander Jochems
+// SPDX-License-Identifier: MIT
+
+package provider
+
+import (
+	"context"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-framework-validators/boolvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/boolplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/listplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/sander0542/nginxproxymanager-go"
+	"github.com/sander0542/terraform-provider-nginxproxymanager/internal/provider/models"
+	"strconv"
+)
+
+var _ resource.Resource = &CertificateLetsencryptResource{}
+var _ resource.ResourceWithImportState = &CertificateLetsencryptResource{}
+
+func NewCertificateLetsencryptResource() resource.Resource {
+	return &CertificateLetsencryptResource{}
+}
+
+type CertificateLetsencryptResource struct {
+	client *nginxproxymanager.APIClient
+	auth   context.Context
+}
+
+func (r *CertificateLetsencryptResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_certificate_letsencrypt"
+}
+
+func (r *CertificateLetsencryptResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "SSL Certificates --- This resource can be used to manage a Let's Encrypt certificate.",
+		Attributes: map[string]schema.Attribute{
+			"id": schema.Int64Attribute{
+				MarkdownDescription: "The ID of the certificate.",
+				Computed:            true,
+				PlanModifiers: []planmodifier.Int64{
+					int64planmodifier.UseStateForUnknown(),
+				},
+			},
+			"created_on": schema.StringAttribute{
+				MarkdownDescription: "The date and time the certificate was created.",
+				Computed:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"modified_on": schema.StringAttribute{
+				MarkdownDescription: "The date and time the certificate was last modified.",
+				Computed:            true,
+			},
+			"expires_on": schema.StringAttribute{
+				MarkdownDescription: "The date and time the certificate expires.",
+				Computed:            true,
+			},
+			"owner_user_id": schema.Int64Attribute{
+				MarkdownDescription: "The Id of the user that owns the certificate.",
+				Computed:            true,
+				PlanModifiers: []planmodifier.Int64{
+					int64planmodifier.UseStateForUnknown(),
+				},
+			},
+			"domain_names": schema.ListAttribute{
+				MarkdownDescription: "The domain names associated with the certificate.",
+				Required:            true,
+				ElementType:         types.StringType,
+				PlanModifiers: []planmodifier.List{
+					listplanmodifier.RequiresReplace(),
+				},
+			},
+			"letsencrypt_email": schema.StringAttribute{
+				MarkdownDescription: "The email address to use for the Let's Encrypt certificate.",
+				Required:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"letsencrypt_agree": schema.BoolAttribute{
+				MarkdownDescription: "Whether you agree to the [Let's Encrypt Terms of Service](https://letsencrypt.org/repository/).",
+				Required:            true,
+				Validators: []validator.Bool{
+					boolvalidator.Equals(true),
+				},
+				PlanModifiers: []planmodifier.Bool{
+					boolplanmodifier.RequiresReplace(),
+				},
+			},
+			"dns_challenge": schema.BoolAttribute{
+				MarkdownDescription: "Whether to use DNS validation to request the Let's Encrypt certificate.",
+				Computed:            true,
+				Optional:            true,
+				Default:             booldefault.StaticBool(false),
+				PlanModifiers: []planmodifier.Bool{
+					boolplanmodifier.RequiresReplace(),
+				},
+			},
+			"dns_provider": schema.StringAttribute{
+				MarkdownDescription: "The DNS provider to use for the DNS challenge.",
+				Computed:            true,
+				Optional:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"dns_provider_credentials": schema.StringAttribute{
+				MarkdownDescription: "The credentials to use for the provider in the DNS challenge.",
+				Computed:            true,
+				Optional:            true,
+				Sensitive:           true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"propagation_seconds": schema.Int64Attribute{
+				MarkdownDescription: "The number of seconds to wait for DNS to propagate before asking the ACME server to verify the DNS record.",
+				Computed:            true,
+				Optional:            true,
+				Sensitive:           true,
+				PlanModifiers: []planmodifier.Int64{
+					int64planmodifier.RequiresReplace(),
+				},
+			},
+		},
+	}
+}
+
+func (r *CertificateLetsencryptResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if auth, client := resourceConfigure(ctx, req, resp); client != nil {
+		r.client = client
+		r.auth = auth
+	}
+}
+
+func (r *CertificateLetsencryptResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data *models.CertificateLetsencrypt
+
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	certificateRequest := data.ToCreateRequest(ctx, &resp.Diagnostics)
+	certificate, _, err := r.client.CertificatesAPI.CreateCertificate(r.auth).CreateCertificateRequest(*certificateRequest).Execute()
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to create certificate, got error: %s", err))
+		return
+	}
+
+	data.Write(ctx, certificate, &resp.Diagnostics)
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *CertificateLetsencryptResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var data *models.CertificateLetsencrypt
+
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	certificate, response, err := r.client.CertificatesAPI.GetCertificate(r.auth, data.Id.ValueInt64()).Execute()
+	if err != nil {
+		if response != nil && response.StatusCode == 404 {
+			resp.State.RemoveResource(ctx)
+			return
+		} else {
+			resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to read certificate, got error: %s", err))
+			return
+		}
+	}
+
+	data.Write(ctx, certificate, &resp.Diagnostics)
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *CertificateLetsencryptResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	resp.Diagnostics.AddError("Client Error", "This resource does not support updates.")
+}
+
+func (r *CertificateLetsencryptResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data *models.CertificateLetsencrypt
+
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	response, _, err := r.client.CertificatesAPI.DeleteCertificate(r.auth, data.Id.ValueInt64()).Execute()
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to delete certificate, got error: %s", err))
+		return
+	}
+
+	if !response {
+		resp.Diagnostics.AddError("Server Error", "Unable to delete certificate.")
+		return
+	}
+}
+
+func (r *CertificateLetsencryptResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	id, err := strconv.ParseInt(req.ID, 10, 64)
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Could not convert ID to number, got error: %s", err))
+		return
+	}
+
+	certificate, _, err := r.client.CertificatesAPI.GetCertificate(r.auth, id).Execute()
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to read certificate, got error: %s", err))
+		return
+	}
+	if certificate.GetProvider() != "letsencrypt" {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("The certificate is not a letsencrypt certificate, got provider: %s", certificate.GetProvider()))
+	}
+
+	diags := resp.State.SetAttribute(ctx, path.Root("id"), types.Int64Value(certificate.GetId()))
+	resp.Diagnostics.Append(diags...)
+}
---
+++ internal/provider/models/certificate_letsencrypt.go
@@ -0,0 +1,113 @@
+// Copyright (c) Sander Jochems
+// SPDX-License-Identifier: MIT
+
+package models
+
+import (
+	"context"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/sander0542/nginxproxymanager-go"
+)
+
+type CertificateLetsencrypt struct {
+	Id          types.Int64  `tfsdk:"id"`
+	CreatedOn   types.String `tfsdk:"created_on"`
+	ModifiedOn  types.String `tfsdk:"modified_on"`
+	ExpiresOn   types.String `tfsdk:"expires_on"`
+	OwnerUserId types.Int64  `tfsdk:"owner_user_id"`
+
+	DomainNames            types.List   `tfsdk:"domain_names"`
+	LetsencryptEmail       types.String `tfsdk:"letsencrypt_email"`
+	LetsencryptAgree       types.Bool   `tfsdk:"letsencrypt_agree"`
+	DnsChallenge           types.Bool   `tfsdk:"dns_challenge"`
+	DnsProvider            types.String `tfsdk:"dns_provider"`
+	DnsProviderCredentials types.String `tfsdk:"dns_provider_credentials"`
+	PropagationSeconds     types.Int64  `tfsdk:"propagation_seconds"`
+}
+
+func (_ CertificateLetsencrypt) GetType() attr.Type {
+	return types.ObjectType{}.WithAttributeTypes(map[string]attr.Type{
+		"id":                       types.Int64Type,
+		"created_on":               types.StringType,
+		"modified_on":              types.StringType,
+		"expires_on":               types.StringType,
+		"owner_user_id":            types.Int64Type,
+		"domain_names":             types.ListType{ElemType: types.StringType},
+		"letsencrypt_email":        types.StringType,
+		"letsencrypt_agree":        types.BoolType,
+		"dns_challenge":            types.BoolType,
+		"dns_provider":             types.StringType,
+		"dns_provider_credentials": types.StringType,
+		"propagation_seconds":      types.Int64Type,
+	})
+}
+
+func (m *CertificateLetsencrypt) Write(ctx context.Context, certificate *nginxproxymanager.GetCertificates200ResponseInner, diags *diag.Diagnostics) {
+	var tmpDiags diag.Diagnostics
+
+	m.Id = types.Int64Value(certificate.GetId())
+	m.CreatedOn = types.StringValue(certificate.GetCreatedOn())
+	m.ModifiedOn = types.StringValue(certificate.GetModifiedOn())
+	m.ExpiresOn = types.StringValue(certificate.GetExpiresOn())
+	m.OwnerUserId = types.Int64Value(certificate.GetOwnerUserId())
+
+	meta := certificate.GetMeta()
+	if meta.HasLetsencryptAgree() {
+		m.LetsencryptAgree = types.BoolValue(meta.GetLetsencryptAgree())
+	} else {
+		m.LetsencryptAgree = types.BoolNull()
+	}
+	if meta.HasLetsencryptEmail() {
+		m.LetsencryptEmail = types.StringValue(meta.GetLetsencryptEmail())
+	} else {
+		m.LetsencryptEmail = types.StringNull()
+	}
+	if meta.HasDnsChallenge() {
+		m.DnsChallenge = types.BoolValue(meta.GetDnsChallenge())
+	} else {
+		m.DnsChallenge = types.BoolNull()
+	}
+	if meta.HasDnsProvider() {
+		m.DnsProvider = types.StringValue(meta.GetDnsProvider())
+	} else {
+		m.DnsProvider = types.StringNull()
+	}
+	if meta.HasDnsProviderCredentials() {
+		m.DnsProviderCredentials = types.StringValue(meta.GetDnsProviderCredentials())
+	} else {
+		m.DnsProviderCredentials = types.StringNull()
+	}
+	if meta.HasPropagationSeconds() {
+		m.PropagationSeconds = types.Int64Value(meta.GetPropagationSeconds())
+	} else {
+		m.PropagationSeconds = types.Int64Null()
+	}
+
+	m.DomainNames, tmpDiags = ListDomainNamesFrom(ctx, certificate.GetDomainNames())
+	diags.Append(tmpDiags...)
+}
+
+func (m *CertificateLetsencrypt) ToCreateRequest(ctx context.Context, diags *diag.Diagnostics) *nginxproxymanager.CreateCertificateRequest {
+	domainNames, tmpDiags := DomainNameElementsAs(ctx, m.DomainNames)
+	diags.Append(tmpDiags...)
+
+	meta := *nginxproxymanager.NewGetCertificates200ResponseInnerMeta()
+	meta.SetLetsencryptEmail(m.LetsencryptEmail.ValueString())
+	meta.SetLetsencryptAgree(m.LetsencryptAgree.ValueBool())
+	meta.SetDnsChallenge(m.DnsChallenge.ValueBool())
+	if m.DnsChallenge.ValueBool() {
+		meta.SetDnsProvider(m.DnsProvider.ValueString())
+		meta.SetDnsProviderCredentials(m.DnsProviderCredentials.ValueString())
+		if !m.PropagationSeconds.IsNull() {
+			meta.SetPropagationSeconds(m.PropagationSeconds.ValueInt64())
+		}
+	}
+
+	request := nginxproxymanager.NewCreateCertificateRequest("letsencrypt")
+	request.SetDomainNames(domainNames)
+	request.SetMeta(meta)
+
+	return request
+}
