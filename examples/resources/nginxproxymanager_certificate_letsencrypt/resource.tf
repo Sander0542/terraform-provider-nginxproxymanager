bdf9474b053e96684daad193796bae5a6c8a7f79
Sander Jochems <sanderjochems@hotmail.nl>
2025-11-17 21:40:41 +0100

    feat: Remove letsencrypt_email and letsencrypt_agree

--- examples/resources/nginxproxymanager_certificate_letsencrypt/resource.tf
+++ examples/resources/nginxproxymanager_certificate_letsencrypt/resource.tf
@@ -1,9 +1,6 @@
 resource "nginxproxymanager_certificate_letsencrypt" "certificate" {
   domain_names = ["example.com", "*.example.com"]
 
-  letsencrypt_email = "admin@example.com"
-  letsencrypt_agree = true
-
   dns_challenge            = true
   dns_provider             = "cloudflare"
   dns_provider_credentials = "dns_cloudflare_api_token=0123456789abcdef0123456789abcdef01234567"
